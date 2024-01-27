12f3bbbfcee88075bb7f4cf35d7e7a0ca74aef9c
Bhavya Singh <bhavyasingh2611@outlook.com>
2024-01-27 12:51:23 +0000

    api stuff

--- backend/README.md
+++ backend/README.md
@@ -1,7 +1,14 @@
 ## API
 
-localhost:6969 <br/>
+localhost:6969/search <br/>
     **/:path** // path to folder (url escaped) <br/>
     **/:folderExclude** // seperated by comma; node_modules, .git <br/>
     **/:fileExclude** // seperated by comma; .gitignore, .gitkeep <br/>
     **/:search** // search string (url escaped) <br/>
+
+---
+
+localhost:6969/tree <br/>
+**/:path** // path to folder (url escaped) <br/>
+**/:folderExclude** // seperated by comma; node_modules, .git <br/>
+**/:fileExclude** // seperated by comma; .gitignore, .gitkeep <br/>
--- backend/index.go
+++ backend/index.go
@@ -21,10 +21,15 @@ type result struct {
 	Line []string `json:"line"`
 }
 
+type fileResult struct {
+	Path string `json:"path"`
+	File string `json:"file"`
+}
+
 func main() {
 	app := fiber.New()
 
-	app.Get("/:path/:folderExclude/:fileExclude/:search", func(c *fiber.Ctx) error {
+	app.Get("/search/:path/:folderExclude/:fileExclude/:search", func(c *fiber.Ctx) error {
 		excludedFolders := strings.Split(c.Params("folderExclude"), ",")
 		excludedFiles := strings.Split(c.Params("fileExclude"), ",")
 
@@ -42,6 +47,26 @@ func main() {
 		search, _ := url.QueryUnescape(c.Params("search"))
 
 		results := fileWalk(path, excludedFoldersHashset, excludedFilesHashset, search)
+		return c.JSON(results)
+	})
+
+	app.Get("/tree/:path/:folderExclude/:fileExclude", func(c *fiber.Ctx) error {
+		excludedFolders := strings.Split(c.Params("folderExclude"), ",")
+		excludedFiles := strings.Split(c.Params("fileExclude"), ",")
+
+		excludedFoldersHashset := hashset.New()
+		for _, excludedFolder := range excludedFolders {
+			excludedFoldersHashset.Add(excludedFolder)
+		}
+
+		excludedFilesHashset := hashset.New()
+		for _, excludedFile := range excludedFiles {
+			excludedFilesHashset.Add(excludedFile)
+		}
+
+		path, _ := url.QueryUnescape(c.Params("path"))
+
+		results := fileTree(path, excludedFoldersHashset, excludedFilesHashset)
 		return c.JSON(results)
 	})
 
@@ -113,6 +138,44 @@ func fileWalk(directory string, excludedFolders *hashset.Set, excludedFiles *has
 			ext := filepath.Ext(fileP)
 			switch ext {
 			}
+		})
+	return results
+}
+
+func fileTree(directory string, excludedFolders *hashset.Set, excludedFiles *hashset.Set) []fileResult {
+	var files []string
+	var results []fileResult
+	err := filepath.WalkDir(directory,
+		func(path string, d fs.DirEntry, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if IsExcluded(d.Name(), excludedFolders) {
+				return filepath.SkipDir
+			}
+			if IsExcludedFile(d.Name(), excludedFiles) {
+				return nil
+			}
+			if !d.IsDir() {
+				files = append(files, path)
+			}
+			return nil
+		})
+	if err != nil {
+		panic(err)
+	}
+
+	iter.ForEach(files,
+		func(file *string) {
+			fileP := strings.Clone(*file)
+			relFileP, _ := filepath.Rel(directory, fileP)
+			baseFileP := filepath.Base(fileP)
+			result := &fileResult{
+				Path: relFileP,
+				File: baseFileP,
+			}
+			results = append(results, *result)
 		})
 	return results
 }
